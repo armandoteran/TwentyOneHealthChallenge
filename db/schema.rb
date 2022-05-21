937313471da32bcdd8583c98ca34c09e94ac6360
armandoteran <ajteranv@gmail.com>
2022-05-21 11:52:00 -0300

    Created state model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_233459) do
+ActiveRecord::Schema.define(version: 2022_05_21_145106) do
+
+  create_table "states", force: :cascade do |t|
+    t.string "full_name"
+    t.string "abbreviation"
+    t.boolean "service_offered"
+    t.integer "minimum_age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
