3ac69c4bcd9b244f15f9e78a0e9a3cb88298ff86
armandoteran <ajteranv@gmail.com>
2022-05-21 20:35:26 -0300

    Creating seeds, not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Deleting States ...'
+State.destroy_all
+puts 'States have been deleted!'
+puts 'Creating states ...'
+State.create(full_name: 'Test')
+puts 'States have been created!'
