9811d4d5874c9f92cd7d24b7f48222812f4670e2
armandoteran <ajteranv@gmail.com>
2022-05-21 11:34:40 -0300

    Created State model and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_233459) do
+ActiveRecord::Schema.define(version: 2022_05_21_143341) do
+
+  create_table "states", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "full_name"
+    t.string "abbreviation"
+    t.boolean "service_offered"
+    t.integer "minimum_age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_states_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_states_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
