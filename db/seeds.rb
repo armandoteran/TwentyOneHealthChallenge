1933b9bb608a8f504aafd39d630dbc9db61d56f2
armandoteran <ajteranv@gmail.com>
2022-05-21 21:38:23 -0300

    Created States seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require_relative '../config/initializers/constants'
+
 puts 'Deleting States ...'
 State.destroy_all
 puts 'States have been deleted!'
-puts 'Creating states ...'
-State.create(full_name: 'Test')
+
+STATES.each do |s|
+  puts 'Creating states ...'
+  State.create(
+    full_name: s[0],
+    abbreviation: s[1],
+    service_offered: s[2],
+    minimum_age: s[3]
+  )
+end
 puts 'States have been created!'
