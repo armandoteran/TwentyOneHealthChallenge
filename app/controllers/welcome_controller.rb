2bc6be1257e047b97105c000c2f1851ff77b9fcf
armandoteran <ajteranv@gmail.com>
2022-05-23 20:54:54 -0300

    Show product ordered if it exits

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    @products = Product.all
+    if user_signed_in? && !current_user.order.nil?
+      @order = Order.find_by(user_id: current_user)
+      @product = Product.find_by(id: current_user.order.product_id)
+    else
+      @products = Product.all
+    end
   end
 end
