d86e86e4e03d08d786666627337af539a5745850
armandoteran <ajteranv@gmail.com>
2022-05-23 19:58:36 -0300

    Completed order create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,23 @@ class OrdersController < ApplicationController
     product_set
   end
 
+  def create
+    @product = Product.find(params[:product_id])
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.product = @product
+    @order.save
+    redirect_to root_path
+  end
+
   private
 
   def product_set
     @product = Product.find(params[:product_id])
     @order = Order.new
+  end
+
+  def order_params
+    params.require(:order).permit(:product_id)
   end
 end
